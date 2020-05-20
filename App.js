7bb1aacaa59ee276e6770d2b95a5161f3583d311
rajathkizhiyedam@gmail.com <rajathkizhiyedam@gmail.com>
2020-05-20 18:48:07 +0530

    ushdfi

--- App.js
+++ App.js
@@ -1,116 +1,13 @@
 import React, { useState } from 'react';
-import {  StyleSheet, Text, View, TextInput, Button, StatusBar, FlatList } from 'react-native';
-import {Icon} from 'react-native-elements';
-import {Snackbar} from 'react-native-paper';
-
-
+import { TouchableOpacity, StyleSheet, Text, View, TextInput, StatusBar, FlatList } from 'react-native';
+import { Icon, colors } from 'react-native-elements';
+import { Snackbar } from 'react-native-paper';
+import FlatListView from './screens/FlatListView'
+import Home from './screens/Home';
 
 export default function App() {
 
-  const [listViewData, setListViewData] = useState(["lamfsd","sfedfdv","sdfkjnsdkf"])
-  const [undoData, setUndoData] = useState()
-  const [newData, setNewData] = useState('')
-  const [deletedData, setDeletedData] = useState('')
-  const [deleted, setDeleted] = useState(false)
-
-
-  const FlatListView=(props)=>{
-    return(<FlatList data={props.data}
-        renderItem={(obj)=>renderItem(obj)}
-        keyExtractor={obj =>obj}
-        />)
-  }
-
-  
-  const TextInputView=(props)=>{
-
-    return(
-      <View style={{alignItems:"center", justifyContent:"space-between", flexDirection:"row",paddingVertical:10,}}>
-
-        
-      <Text>{props.item} </Text>
-        <View  style={{ justifyContent:"flex-end", flexDirection:"row" }}>
-          <Icon name="check" type="feather" onPress={()=>{
-            setUndoData(listViewData)
-            setListViewData(listViewData.filter(x=>(x!==props.item)))
-            setDeletedData(props.item)
-            setDeleted(true)
-        }}/>
-        </View>
-        </View>
-    )
-  }
-
-
-  const renderItem = (obj)=>{
-
-    return(<View >
-      <TextInputView item={obj.item}></TextInputView>
-
-    </View>)
-
-  }
-  
   return(
-    <View style={styles.container}>
-      
-      <View style={{flex:1, padding:10}}>
-      <FlatListView data={listViewData}></FlatListView>
-      </View>
-        <Snackbar
-        style={{marginBottom:100, alignSelf:"center", marginLeft:27}}
-          visible={deleted}
-          onDismiss={()=>{
-            setDeleted(false)
-          }}
-          duration={1500}
-          action={{
-            label: 'Undo',
-            onPress: () => {
-              // setDeleted(false)
-              setListViewData(undoData)
-              setDeleted(false)
-            },
-          }}
-        >
-          {deletedData} is removed from list
-        </Snackbar>
-      
-      <View style={{justifyContent:"space-between", flexDirection:"row", flex:0, paddingTop:20, paddingHorizontal:10, paddingBottom:10}}>
-        <TextInput value={newData} onChangeText={text=>{
-          setNewData(text)
-        }} placeholder="Create your new item" multiline></TextInput>
-        <View >
-          <Icon name="plus" type="feather" onPress={()=>{
-            if(newData)
-            {
-              a=listViewData.filter(x=>(x===newData))
-              console.log(a.length)
-              if(a.length===0){
-                setListViewData([...listViewData, newData])
-              }
-              else{
-                alert(newData + " Already exists")
-              }
-          }
-          setNewData('')
-        
-        }}></Icon>
-        </View>
-      </View>
-      
-
-
-    </View>
+    <Home></Home>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop:StatusBar.currentHeight,
-    padding:10,
-    marginTop:30,
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
---
+++ screens/FlatListView.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Text, View, FlatList, TextInput } from 'react-native';
+import {Icon} from 'react-native-elements';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const TextInputView=(props)=>{
+  const lightGrey='#fafaf6';
+  const darkBlue="#2d4059"
+  const goldenYellow='#ffd717'   //#07d5c0   //#c6e872   //#fbffa3
+  const [selectionColor, setSelectionColor] = useState(lightGrey)
+  const [textColor, setTextColor] = useState(darkBlue)
+  return(
+    <View style={{alignItems:"center", justifyContent:"space-between", flexDirection:"row", borderRadius:8, backgroundColor:selectionColor, paddingVertical:10, paddingHorizontal:16, width:"100%"}}>
+      
+      <View style={{width:"90%"}}>
+        <TouchableOpacity onPress={()=>{
+          props.onTouch(props.item)
+        }}>
+            <Text style={{fontSize:15, color:textColor}}>{props.item}</Text>
+            
+        </TouchableOpacity>
+      </View>
+        <View style={{ justifyContent:"flex-end", flexDirection:"row", width:"10%"}}>
+          <View>
+            {(selectionColor===lightGrey) &&
+            <Icon name="circle" type="feather" color={darkBlue} onPress={()=>{
+                setSelectionColor(darkBlue)
+                setTextColor('white')
+            }}/>
+          }
+          {(selectionColor!==lightGrey) &&
+            <Icon name="check-circle" type="feather" color={goldenYellow} onPress={()=>{
+                setSelectionColor(lightGrey)
+                setTextColor(darkBlue)
+            }}/>
+          }
+          </View>
+        </View>
+    </View>
+  )
+}
+
+
+const renderItem = (obj,onTouch)=>{
+  return(
+      <View style={{marginVertical:5}}>
+        <TextInputView item={obj.item} onTouch={onTouch} ></TextInputView>
+      </View>
+  )
+
+}
+const FlatListView=(props)=>{
+  return(<FlatList data={props.data}
+      renderItem={(obj)=>renderItem(obj,props.onTouch)}
+      keyExtractor={obj =>obj}
+      />)
+}
+
+export default FlatListView
